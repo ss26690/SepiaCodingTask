3f425ee5c908947154b19fea49d277108b1c35f2
Saurav Sagar <sagarsaurav1997@gmail.com>
2023-02-26 11:29:17 +0530

    Project Initial Setup

---
+++ SepiaCodingTask/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SepiaCodingTask
+//
+//  Created by Saurav Sagar on 25/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        var number: NSNumber = 1
+        var result  = number as? Int
+        debugPrint(result!)
+        
+        
+    }
+}
